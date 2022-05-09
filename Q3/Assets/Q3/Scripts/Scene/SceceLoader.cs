3847dabdb9f14a4c36a3f36a0531b037e5ff6126
g_garden <dragon.8783@gmail.com>
2022-05-10 00:32:54 +0900

    チラつくので設定変えてみる

--- Q3/Assets/Q3/Scripts/Scene/SceceLoader.cs
+++ Q3/Assets/Q3/Scripts/Scene/SceceLoader.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SceceLoader : MonoBehaviour
 {
+    private void Start()
+    {
+        Application.targetFrameRate = 60;
+    }
+
     public void LoadScene(string name)
     {
         SceneManager.LoadScene(name);
